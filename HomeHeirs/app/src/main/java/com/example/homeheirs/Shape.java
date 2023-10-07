3a43f21dddeada5e684f14c0e81fdd8e159b90bf
rayyanrashid02 <90708043+rayyanrashid02@users.noreply.github.com>
2023-10-06 18:26:55 -0600

    Step 3 - Added Color

--- HomeHeirs/app/src/main/java/com/example/homeheirs/Shape.java
+++ HomeHeirs/app/src/main/java/com/example/homeheirs/Shape.java
@@ -3,6 +3,8 @@ package com.example.homeheirs;
 public abstract class Shape {
     public int x, y;
 
+    String color = "Green";
+
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
