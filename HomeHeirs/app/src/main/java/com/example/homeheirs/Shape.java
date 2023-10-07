efddcb148a20d9fbc29f65a45366e30e15cfd6fd
rayyanrashid02 <90708043+rayyanrashid02@users.noreply.github.com>
2023-10-06 19:01:13 -0600

    Reset to Step 2

--- HomeHeirs/app/src/main/java/com/example/homeheirs/Shape.java
+++ HomeHeirs/app/src/main/java/com/example/homeheirs/Shape.java
@@ -3,8 +3,6 @@ package com.example.homeheirs;
 public abstract class Shape {
     public int x, y;
 
-    String color = "Blue";
-
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
