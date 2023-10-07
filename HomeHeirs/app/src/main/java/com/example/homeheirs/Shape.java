94494a3892cb8122af486b7b62ebcec8dc33870f
rayyanrashid02 <rashid4@ualberta.ca>
2023-10-06 18:49:57 -0600

    Add color to Shape

--- HomeHeirs/app/src/main/java/com/example/homeheirs/Shape.java
+++ HomeHeirs/app/src/main/java/com/example/homeheirs/Shape.java
@@ -3,7 +3,7 @@ package com.example.homeheirs;
 public abstract class Shape {
     public int x, y;
 
-    String color = "Green";
+    String color = "Blue";
 
     public Shape(int x, int y){
         this.x = x;
