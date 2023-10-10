130b1498fefd9e6170b4bc50ff7cc878a3b910a8
arsalanahmed2158 <96898972+arsalanahmed2158@users.noreply.github.com>
2023-10-10 00:20:27 -0600

    Update Shape.java

--- HomeHeirs/app/src/main/java/com/example/homeheirs/Shape.java
+++ HomeHeirs/app/src/main/java/com/example/homeheirs/Shape.java
@@ -2,7 +2,7 @@ package com.example.homeheirs;
 
 public abstract class Shape {
     public int x, y;
-
+    String color = "yellow";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
