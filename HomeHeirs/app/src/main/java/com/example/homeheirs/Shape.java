faeb1f064cdc35aa9608f7888114ac155ce5b4db
Archi Patel <archi1@ualberta.ca>
2023-10-10 00:24:09 -0600

    removed constructer and added blue string

--- HomeHeirs/app/src/main/java/com/example/homeheirs/Shape.java
+++ HomeHeirs/app/src/main/java/com/example/homeheirs/Shape.java
@@ -2,9 +2,5 @@ package com.example.homeheirs;
 
 public abstract class Shape {
     public int x, y;
-
-    public Shape(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
+    public String colour = "blue";
 }
