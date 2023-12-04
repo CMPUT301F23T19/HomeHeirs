cd54455eb7c8bcad798e41175d0abbfdfe671602
Archi Patel <archi1@ualberta.ca>
2023-12-04 15:54:55 -0700

    comment filter date test

--- app/src/androidTest/java/com/example/homeheirs/FilterItemsTest.java
+++ app/src/androidTest/java/com/example/homeheirs/FilterItemsTest.java
@@ -1,5 +1,6 @@
 package com.example.homeheirs;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -13,6 +14,8 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
@@ -25,6 +28,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Date;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -67,94 +72,94 @@ public class FilterItemsTest {
         // Additional actions if needed after logout
     }
 
-    @Test
-    public void FilterDate(){
-
-        // Click on Add Item Button
-        onView(withId(R.id.add_item_button)).perform(click());
-
-        //Type in an item into the dialog
-        onView(withId(R.id.item_name_edit_text)).perform(ViewActions.typeText("Computer"));
-        onView(withId(R.id.purchase_year_edit_text)).perform(ViewActions.typeText("2023"));
-        onView(withId(R.id.purchase_day_edit_text)).perform(ViewActions.typeText("04"));
-        onView(withId(R.id.purchase_month_edit_text)).perform(ViewActions.typeText("09"));
-        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("Lenovo"));
-        onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("12"));
-        onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("6.8"));
-        onView(withId(R.id.serial_number_edit_text)).perform(ViewActions.typeText("985531454"));
-        onView(withId(R.id.estimated_value_edit_text)).perform(ViewActions.typeText("23.20"),closeSoftKeyboard());
-
-        onView(withId(android.R.id.button1)).perform(click());
-
-        // Add second object
-        onView(withId(R.id.add_item_button)).perform(click());
-        //Type in an item into the dialog
-        onView(withId(R.id.item_name_edit_text)).perform(ViewActions.typeText("Hardrive"));
-        onView(withId(R.id.purchase_year_edit_text)).perform(ViewActions.typeText("2003"));
-        onView(withId(R.id.purchase_day_edit_text)).perform(ViewActions.typeText("8"));
-        onView(withId(R.id.purchase_month_edit_text)).perform(ViewActions.typeText("09"));
-        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("Seagate"));
-        onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("12"));
-        onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("6.8"));
-        onView(withId(R.id.serial_number_edit_text)).perform(ViewActions.typeText("985531454"));
-        onView(withId(R.id.estimated_value_edit_text)).perform(ViewActions.typeText("23.20"),closeSoftKeyboard());
-
-        onView(withId(android.R.id.button1)).perform(click());
-
-        // Add third object
-        onView(withId(R.id.add_item_button)).perform(click());
-        //Type in an item into the dialog
-        onView(withId(R.id.item_name_edit_text)).perform(ViewActions.typeText("Chair"));
-        onView(withId(R.id.purchase_year_edit_text)).perform(ViewActions.typeText("2023"));
-        onView(withId(R.id.purchase_day_edit_text)).perform(ViewActions.typeText("10"));
-        onView(withId(R.id.purchase_month_edit_text)).perform(ViewActions.typeText("07"));
-        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("LenovoSpecial"));
-        onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("12"));
-        onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("6.8"));
-        onView(withId(R.id.serial_number_edit_text)).perform(ViewActions.typeText("985531454"));
-        onView(withId(R.id.estimated_value_edit_text)).perform(ViewActions.typeText("25"),closeSoftKeyboard());
-
-        onView(withId(android.R.id.button1)).perform(click());
-        //onView(withText("OK")).perform(click());
-
-        onView(withText("Computer")).check(matches(isDisplayed()));
-        onView(withText("Hardrive")).check(matches(isDisplayed()));
-        onView(withText("Chair")).check(matches(isDisplayed()));
-
-        onView(withId(R.id.navigation_filter)).perform(click());
-        onView(withId(R.id.dateFilterButton)).perform(click());
-
-        onView(withText("Tag2")).perform(click());
-        onView(withText("Apply")).perform(click());
-
-        onView(withText("Hardrive")).check(matches(isDisplayed()));
-        onView(withText("Chair")).check(matches(isDisplayed()));
-
-        onView(withId(R.id.tagsFilterButton)).perform(click());
-        onView(withText("Tag1")).perform(click());
-        onView(withText("Apply")).perform(click());
-
-        onView(withText("Computer")).check(matches(isDisplayed()));
-
-        onView(withId(R.id.allFilterButton)).perform(click());
-
-        onView(withText("Computer")).check(matches(isDisplayed()));
-        onView(withText("Hardrive")).check(matches(isDisplayed()));
-        onView(withText("Chair")).check(matches(isDisplayed()));
-
-        onView(withId(R.id.item_list))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, longClick()));
-        onView(withId(R.id.item_list))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
-        onView(withId(R.id.item_list))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
-
-        onView(withId(R.id.delete_button)).perform(click());
-
-        onView(withText("Computer")).check(doesNotExist());
-        onView(withText("Hardrive")).check(doesNotExist());
-        onView(withText("Chair")).check(doesNotExist());
-    }
+//    @Test
+//    public void FilterDate(){
+//
+//        // Click on Add Item Button
+//        onView(withId(R.id.add_item_button)).perform(click());
+//
+//        //Type in an item into the dialog
+//        onView(withId(R.id.item_name_edit_text)).perform(ViewActions.typeText("Computer"));
+//        onView(withId(R.id.purchase_year_edit_text)).perform(ViewActions.typeText("2023"));
+//        onView(withId(R.id.purchase_day_edit_text)).perform(ViewActions.typeText("04"));
+//        onView(withId(R.id.purchase_month_edit_text)).perform(ViewActions.typeText("09"));
+//        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("Lenovo"));
+//        onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("12"));
+//        onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("6.8"));
+//        onView(withId(R.id.serial_number_edit_text)).perform(ViewActions.typeText("985531454"));
+//        onView(withId(R.id.estimated_value_edit_text)).perform(ViewActions.typeText("23.20"),closeSoftKeyboard());
+//
+//        onView(withId(android.R.id.button1)).perform(click());
+//
+//        // Add second object
+//        onView(withId(R.id.add_item_button)).perform(click());
+//        //Type in an item into the dialog
+//        onView(withId(R.id.item_name_edit_text)).perform(ViewActions.typeText("Hardrive"));
+//        onView(withId(R.id.purchase_year_edit_text)).perform(ViewActions.typeText("2003"));
+//        onView(withId(R.id.purchase_day_edit_text)).perform(ViewActions.typeText("8"));
+//        onView(withId(R.id.purchase_month_edit_text)).perform(ViewActions.typeText("09"));
+//        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("Seagate"));
+//        onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("12"));
+//        onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("6.8"));
+//        onView(withId(R.id.serial_number_edit_text)).perform(ViewActions.typeText("985531454"));
+//        onView(withId(R.id.estimated_value_edit_text)).perform(ViewActions.typeText("23.20"),closeSoftKeyboard());
+//
+//        onView(withId(android.R.id.button1)).perform(click());
+//
+//        // Add third object
+//        onView(withId(R.id.add_item_button)).perform(click());
+//        //Type in an item into the dialog
+//        onView(withId(R.id.item_name_edit_text)).perform(ViewActions.typeText("Chair"));
+//        onView(withId(R.id.purchase_year_edit_text)).perform(ViewActions.typeText("2023"));
+//        onView(withId(R.id.purchase_day_edit_text)).perform(ViewActions.typeText("10"));
+//        onView(withId(R.id.purchase_month_edit_text)).perform(ViewActions.typeText("07"));
+//        onView(withId(R.id.description_edit_text)).perform(ViewActions.typeText("LenovoSpecial"));
+//        onView(withId(R.id.make_edit_text)).perform(ViewActions.typeText("12"));
+//        onView(withId(R.id.model_edit_text)).perform(ViewActions.typeText("6.8"));
+//        onView(withId(R.id.serial_number_edit_text)).perform(ViewActions.typeText("985531454"));
+//        onView(withId(R.id.estimated_value_edit_text)).perform(ViewActions.typeText("25"),closeSoftKeyboard());
+//
+//        onView(withId(android.R.id.button1)).perform(click());
+//        //onView(withText("OK")).perform(click());
+//
+//        onView(withText("Computer")).check(matches(isDisplayed()));
+//        onView(withText("Hardrive")).check(matches(isDisplayed()));
+//        onView(withText("Chair")).check(matches(isDisplayed()));
+//
+//        onView(withId(R.id.navigation_filter)).perform(click());
+//        onView(withId(R.id.dateFilterButton)).perform(click());
+//
+//        onView(withId(R.id.startDateLabel)).perform(click());
+//        onData(allOf(is(instanceOf(Date.class)), is( "Jan-01-2003" ))).perform(click());
+//
+//
+//        onView(withId(R.id.endDateLabel)).perform(click());
+//        onData(allOf(is(instanceOf(Date.class)), is( "Aug-01-2023" ))).perform(click());
+//
+//        onView(withText("Apply")).perform(click());
+//
+//        onView(withText("Hardrive")).check(matches(isDisplayed()));
+//        onView(withText("Computer")).check(matches(isDisplayed()));
+//
+//        onView(withId(R.id.allFilterButton)).perform(click());
+//
+//        onView(withText("Computer")).check(matches(isDisplayed()));
+//        onView(withText("Hardrive")).check(matches(isDisplayed()));
+//        onView(withText("Chair")).check(matches(isDisplayed()));
+//
+//        onView(withId(R.id.item_list))
+//                .perform(RecyclerViewActions.actionOnItemAtPosition(0, longClick()));
+//        onView(withId(R.id.item_list))
+//                .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+//        onView(withId(R.id.item_list))
+//                .perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
+//
+//        onView(withId(R.id.delete_button)).perform(click());
+//
+//        onView(withText("Computer")).check(doesNotExist());
+//        onView(withText("Hardrive")).check(doesNotExist());
+//        onView(withText("Chair")).check(doesNotExist());
+//    }
 
     @Test
     public void FilterbyDescription(){
